c7436991230e8c675aa3dd136f3363be1c76c4ee
bLu.R <blu.r@outlook.com>
2022-10-10 21:00:58 -0500

    Some color/alignment fixes and store the user preference added

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-console.log("kame house");
 const toggle = document.querySelector(".toggle");
+const hr = document.querySelector(".header__hr");
 const toggleCircle = document.querySelector(".toggle__circle");
+const currentTheme = localStorage.getItem("theme")
+    ? localStorage.getItem("theme")
+    : null;
 
 let on = true;
 const toggleTheme = () => {
@@ -8,12 +11,16 @@ const toggleTheme = () => {
         document.documentElement.setAttribute("data-theme", "light");
         toggle.classList.remove("toggle-bg-gradient");
         toggleCircle.classList.add("toggle__circle-on");
+        hr.style.opacity = 1;
+        localStorage.setItem("theme", "light");
 
         on = false;
     } else {
         document.documentElement.setAttribute("data-theme", "dark");
         toggle.classList.add("toggle-bg-gradient");
         toggleCircle.classList.remove("toggle__circle-on");
+        hr.style.opacity = 0.2;
+        localStorage.setItem("theme", "dark");
 
         on = true;
     }
@@ -29,6 +36,10 @@ const out = () => {
         toggle.classList.remove("toggle-bg-gradient");
     }
 };
+
+if (currentTheme === "light") {
+    toggleTheme();
+}
 
 toggle.addEventListener("mouseover", over, false);
 toggle.addEventListener("mouseout", out, false);
